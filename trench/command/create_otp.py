861abba261adf7801437f24ff9ead1d80e8c2720
Gerben <info@gerbenvaneerten.nl>
2022-04-12 16:55:51 +0200

    Removed interval=1 parameter

--- trench/command/create_otp.py
+++ trench/command/create_otp.py
@@ -4,7 +4,7 @@ from pyotp import TOTP
 class CreateOTPCommand:
     @staticmethod
     def execute(secret: str) -> TOTP:
-        return TOTP(secret, interval=1)
+        return TOTP(secret)
 
 
 create_otp_command = CreateOTPCommand.execute
